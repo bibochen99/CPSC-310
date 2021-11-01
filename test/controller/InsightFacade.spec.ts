d92558af9840f5582d77a0dad9648aa65b2ec319
Roy <zhongroy189@gmail.com>
2021-11-01 16:09:22 -0700

    update typo

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -82,8 +82,8 @@ describe("InsightFacade_given", function () {
 			// Load the datasets specified in datasetsToQuery and add them to InsightFacade.
 			// Will *fail* if there is a problem reading ANY dataset.
 			const loadDatasetPromises = [
-				// insightFacade.addDataset("courses", datasetContents.get("courses") ?? ""
-				// 	, InsightDatasetKind.Courses),
+				insightFacade.addDataset("courses", datasetContents.get("courses") ?? ""
+					, InsightDatasetKind.Courses),
 				insightFacade.addDataset("rooms", datasetContents.get("rooms") ?? ""
 					, InsightDatasetKind.Rooms)
 			];
